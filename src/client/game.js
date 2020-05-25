2bb39ccca65314c4a190c930e21560d72c8f09c1
Jean-Philippe Bureau <jeanphilippe.bureau1@gmail.com>
2020-05-25 19:36:55 -0400

    docker implementation WIP

--- src/client/game.js
+++
@@ -1,5 +0,0 @@
-let i = 2;
-
-let y = i + 2;
-
-console.log("test");
--- src/server/server.ts
+++ src/server/server.ts
@@ -26,7 +26,8 @@ export class Server {
 
   private handleRoutes(): void {
     this.app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname + "index.html"));
+      //res.sendFile(path.join(__dirname, "../", "/client/index.html"));
+      res.send({ object: "test" });
     });
   }
 
