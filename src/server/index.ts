e09b6be8813a5fdabfbe48c5b2533ab994a6cb5e
Jean-Philippe Bureau <jeanphilippe.bureau1@gmail.com>
2020-06-19 16:58:32 -0400

    Removed unused declaration

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,8 +2,6 @@ import express from "express";
 import { createServer } from "http";
 import path from "path";
 import socketIO from "socket.io";
-import GameFSM from "../GameCore/gameState/gameFSM";
-import PreGame from "../GameCore/gameState/preGame";
 import applyCommonContext, { serviceConstants } from "./context/commonContext";
 import ServiceLocator from "./context/serviceLocator";
 import GameManager from "./gameManager";
@@ -31,8 +29,6 @@ const server = new Server(app, httpServer);
 server.listen((port) => {
 	console.log(`Server is listening on http://localhost:${port}`);
 });
-
-const fsm1 = new GameFSM(new PreGame(), ServiceLocator.resolve(serviceConstants.GAME_REPOSITORY_CLASS));
 
 // à voir le nom de la méthode si on prend update() directement
 gameManager.startLoop();
