32070dcdf34c158d30d67241e85fc04b53c5c77c
Jean-Philippe Bureau <jeanphilippe.bureau1@gmail.com>
2020-07-03 10:08:41 -0400

    client socket added to client game controller

--- src/GameCore/Race/ClientRaceGameController.ts
+++ src/GameCore/Race/ClientRaceGameController.ts
@@ -6,12 +6,24 @@ import RaceGrid from "./RaceGrid";
 
 export default class ClientRaceGameController extends RaceGameController implements ClientGame {
 	private currentPlayerId: string;
-	constructor(gameTime: number, gameTimeStamp: number, grid: RaceGrid, players: Player[], currentPlayerId: string) {
+	private playerSocket: SocketIOClient.Socket;
+
+	constructor(
+		gameTime: number,
+		gameTimeStamp: number,
+		grid: RaceGrid,
+		players: Player[],
+		currentPlayerId: string,
+		playerSocket: SocketIOClient.Socket
+	) {
 		super(gameTime, gameTimeStamp, grid, players);
 		this.currentPlayerId = currentPlayerId;
+		this.playerSocket = playerSocket;
 	}
 
 	public update(): void {}
 
 	public setGameState(gameState: RaceGameState) {}
+
+	public itemUsedOn() {}
 }
