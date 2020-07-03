2dab14a8ff94e4acbe975e0515312c3a0cadb007
Samuel Gagnon <samuel.gagnon@mat.ulaval.ca>
2020-07-03 10:40:09 -0400

    folder restructure

--- game/src/client/scenes/menuScene.ts
+++ game/src/client/scenes/menuScene.ts
@@ -2,23 +2,22 @@ import { CST } from "../CST";
 
 export default class MenuScene extends Phaser.Scene {
 	constructor() {
-		const sceneConfig = { key: CST.SCENES.MENU };
+		const sceneConfig = {
+			key: CST.SCENES.MENU,
+		};
 		super(sceneConfig);
 	}
 
 	create() {
-		this.add
-			.tileSprite(0, 0, Number(this.game.config.width), Number(this.game.config.height), CST.IMAGES.BACKGROUD)
-			.setOrigin(0)
-			.setDepth(0);
+		this.add.tileSprite(0, 0, Number(this.game.config.width), Number(this.game.config.height), CST.IMAGES.BACKGROUD).setOrigin(0).setDepth(0);
 
 		this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 0.2, CST.IMAGES.TITLE).setDepth(1);
 
-		let playButton = this.add
-			.image(this.game.renderer.width / 2, this.game.renderer.height / 2, CST.IMAGES.PLAY)
-			.setDepth(1);
+		let playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, CST.IMAGES.PLAY).setDepth(1);
 
-		playButton.setInteractive({ useHandCursor: true });
+		playButton.setInteractive({
+			useHandCursor: true,
+		});
 
 		playButton.on("pointerover", () => {
 			playButton.setTint(0xffff66);
