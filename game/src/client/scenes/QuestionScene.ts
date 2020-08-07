00f84131f37068cb142725fb68f6db0c41e442ac
Jean-Philippe Bureau <jeanphilippe.bureau1@gmail.com>
2020-08-07 14:08:55 -0400

    book and crystal ball item added

--- game/src/client/scenes/QuestionScene.ts
+++ game/src/client/scenes/QuestionScene.ts
@@ -21,6 +21,11 @@ export default class QuestionScene extends Phaser.Scene {
 	enterButton: Phaser.GameObjects.Text;
 	inputHtml: Phaser.GameObjects.DOMElement;
 
+	bookIcon: Phaser.GameObjects.Sprite;
+	crystalBallIcon: Phaser.GameObjects.Sprite;
+	bookCount: Phaser.GameObjects.Text;
+	crystalBallCount: Phaser.GameObjects.Text;
+
 	feedbackMaxTime: number;
 	feedbackStartTimeStamp: number;
 	feedbackRemainingTime: Phaser.GameObjects.Text;
@@ -45,6 +50,77 @@ export default class QuestionScene extends Phaser.Scene {
 		this.cameras.main.setViewport(this.position.x, this.position.y, this.width, this.height);
 		this.cameras.main.setBackgroundColor(0xffffff);
 
+		this.inputHtml = this.add.dom(this.width * 0.4, this.height * 0.85).createFromCache(CST.HTML.ANSWER_INPUT);
+
+		this.enterButton = this.add
+			.text(this.width * 0.6, this.height * 0.85, "enter", {
+				fontFamily: "Courier",
+				fontSize: "32px",
+				align: "center",
+				color: "#000000",
+				fontStyle: "bold",
+			})
+			.setScrollFactor(0);
+
+		this.enterButton.setInteractive({
+			useHandCursor: true,
+		});
+
+		this.enterButton.on("pointerup", () => {
+			this.answerQuestion();
+		});
+
+		this.bookIcon = this.add
+			.sprite(this.width * 0.05, this.height * 0.9, CST.IMAGES.BOOK)
+			.setInteractive({ useHandCursor: true })
+			.setScale(0.1);
+		this.crystalBallIcon = this.add
+			.sprite(this.width * 0.15, this.height * 0.9, CST.IMAGES.CRYSTAL_BALL)
+			.setInteractive({ useHandCursor: true })
+			.setScale(0.1);
+
+		this.bookIcon.on("pointerdown", () => {
+			this.bookIcon.setTint(0xffea00);
+		});
+		this.bookIcon.on("pointerover", () => {
+			this.bookIcon.clearTint();
+		});
+		this.bookIcon.on("pointerup", () => {
+			this.bookIcon.clearTint();
+			console.log("book");
+		});
+
+		this.crystalBallIcon.on("pointerdown", () => {
+			this.crystalBallIcon.setTint(0xffea00);
+		});
+		this.crystalBallIcon.on("pointerover", () => {
+			this.crystalBallIcon.clearTint();
+		});
+		this.crystalBallIcon.on("pointerup", () => {
+			this.crystalBallIcon.clearTint();
+			console.log("crystal ball");
+		});
+
+		this.bookCount = this.add
+			.text(this.bookIcon.getTopRight().x - 7, this.bookIcon.getTopRight().y, "0", {
+				fontFamily: "Arial",
+				fontSize: "20px",
+				align: "center",
+				color: "#000000",
+				fontStyle: "bold",
+			})
+			.setScrollFactor(0);
+
+		this.crystalBallCount = this.add
+			.text(this.crystalBallIcon.getTopRight().x + 5, this.crystalBallIcon.getTopRight().y, "0", {
+				fontFamily: "Arial",
+				fontSize: "20px",
+				align: "center",
+				color: "#000000",
+				fontStyle: "bold",
+			})
+			.setScrollFactor(0);
+
 		this.textures.on(
 			"addtexture",
 			(textureId: string) => {
@@ -53,25 +129,6 @@ export default class QuestionScene extends Phaser.Scene {
 					console.log(`width: ${this.questionImage.displayWidth},  height: ${this.questionImage.displayHeight}`);
 					if (this.questionImage.displayHeight > 500 && this.questionImage.displayHeight < 800) this.questionImage.setScale(0.2);
 					if (this.questionImage.displayHeight >= 800) this.questionImage.setScale(0.15);
-					this.inputHtml = this.add.dom(this.width * 0.4, this.height * 0.85).createFromCache(CST.HTML.ANSWER_INPUT);
-
-					this.enterButton = this.add
-						.text(this.width * 0.6, this.height * 0.85, "enter", {
-							fontFamily: "Courier",
-							fontSize: "32px",
-							align: "center",
-							color: "#000000",
-							fontStyle: "bold",
-						})
-						.setScrollFactor(0);
-
-					this.enterButton.setInteractive({
-						useHandCursor: true,
-					});
-
-					this.enterButton.on("pointerup", () => {
-						this.answerQuestion();
-					});
 				} else if (textureId === "feedback") {
 					this.feedbackImage = this.add
 						.image(this.width * 0.5, this.height * 0.35, "feedback")
@@ -91,6 +148,10 @@ export default class QuestionScene extends Phaser.Scene {
 		if (this.showFeedbackTime) {
 			this.feedbackRemainingTime.setText(Math.ceil((this.feedbackStartTimeStamp - Date.now() + this.feedbackMaxTime) / 1000).toString());
 		}
+
+		const inventoryState = (<RaceScene>this.scene.get(CST.SCENES.RACE_GAME)).raceGame.getCurrentPlayer().getInventory().getInventoryState();
+		this.bookCount.setText(inventoryState.bookCount.toString());
+		this.crystalBallCount.setText(inventoryState.crystalBallCount.toString());
 	}
 
 	private getTexturesForQuestion(): void {
