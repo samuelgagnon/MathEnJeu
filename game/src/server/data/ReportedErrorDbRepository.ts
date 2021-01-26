b0125cc5dd97627a0cf92661ed6f44f005250788
Samuel Gagnon <samuel.gagnon@mat.ulaval.ca>
2021-01-26 11:10:03 -0500

    Added errorLog in addReportedError

--- game/src/server/data/ReportedErrorDbRepository.ts
+++ game/src/server/data/ReportedErrorDbRepository.ts
@@ -4,16 +4,18 @@ import ReportedErrorRepository from "./ReportedErrorRepository";
 export default class ReportedErrorDbRepository implements ReportedErrorRepository {
 	constructor() {}
 
-	addReportedError(languageShortName: string, errorDescription: string, questionId?: number, username?: string): void {
-		const questionId_string = questionId === undefined ? "NULL" : String(questionId);
-		const username_string = username === undefined ? "NULL" : `'${username}'`;
-		const queryString = `INSERT INTO reported_error ( language_id, description, question_id, username)
+	addReportedError(languageShortName: string, errorDescription: string, questionId?: number, username?: string, errorLog?: string): void {
+		const questionIdString = questionId === undefined ? "NULL" : String(questionId);
+		const usernameString = username === undefined ? "NULL" : `'${username}'`;
+		const errorLogString = errorLog === undefined ? "NULL" : `'${errorLog}'`;
+		const queryString = `INSERT INTO reported_error ( language_id, description, question_id, username, app_error_log)
 		VALUES ((SELECT language_id
 			FROM \`language\`
 			WHERE \`language\`.short_name LIKE '${languageShortName}'), 
             '${errorDescription}',
-            ${questionId_string},
-			${username_string});`;
+            ${questionIdString},
+            ${usernameString},
+            ${errorLogString});`;
 
 		getConnection().query(queryString);
 	}
