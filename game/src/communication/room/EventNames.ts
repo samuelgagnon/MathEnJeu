9b734ac09f0596b3b6a797b60a74cbfece57f3ab
Samuel Gagnon <samuel.gagnon@mat.ulaval.ca>
2021-02-22 13:21:37 -0500

    changed event names

--- game/src/communication/room/EventNames.ts
+++ game/src/communication/room/EventNames.ts
@@ -1,7 +1,7 @@
 export const ROOM_EVENT_NAMES = {
 	CREATE_ROOM: "create-room",
-	JOIN_ROOM_REQUEST: "join-room",
-	JOIN_ROOM_ANSWER: "room-joined",
+	JOIN_ROOM_REQUEST: "join-room-request",
+	JOIN_ROOM_ANSWER: "join-room-answer",
 	HOST_CHANGE: "host-change",
 	IS_HOST: "is-host",
 };
