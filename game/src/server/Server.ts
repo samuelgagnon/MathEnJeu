dc941302e0a3e4830c4673036153a13f746654fd
Jean-Philippe Bureau <54918408+Jpbureau@users.noreply.github.com>
2021-03-23 11:44:42 -0400

    Feature/latex to html questions (#64)
    * staging environment test
    
    * staging environment test
    
    * staging environment test
    
    * staging environment test
    
    * staging environment test
    
    * staging environment test
    
    * staging environment test
    
    * staging environment test
    
    * Changed max number of player
    
    * git ingore modified for questions
    
    * allowed access to svg images in questions
    
    * git ignore updated
    
    * latex files generation
    
    * git ignore updated
    
    * todo removed schoolGrandId from request
    
    * docker file update and JSDOM added to dep
    
    * git ignore updated
    
    * script appends html to make html files functionnal
    
    * script to transfer answers to latex files
    
    * folder name changes
    
    * WIP
    
    * added lines for blank answers
    
    * WIP
    
    * questions boxes implemented
    
    * console log question id for debug
    
    * added continue button and feedback is time finished
    
    * bug fix
    
    * changes made for pull request
    
    * bug fixed where book and crystal ball couldn't be used
    
    * Removed unused code
    
    Co-authored-by: Samuel Gagnon <samuel.gagnon@mat.ulaval.ca>

--- game/src/server/Server.ts
+++ game/src/server/Server.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
 import { Application } from "express";
+import fs from "fs";
 import { Server as HTTPServer } from "http";
+import { JSDOM } from "jsdom";
 import path from "path";
 import "reflect-metadata";
 import ErrorReport from "../communication/ErrorReport";
@@ -35,36 +37,36 @@ export class Server {
 			res.sendFile(path.join(__dirname, "../", "/client/index.html"));
 		});
 
-		this.app.get("/questionImage", async (req, res) => {
-			const questionId = req.query.id;
-			const languageShortName = req.query.languageShortName;
-			const schoolGradeId = req.query.schoolGradeId;
-
-			this.questionRepo
-				.getQuestionById(Number(questionId), languageShortName.toString(), Number(schoolGradeId))
-				.then((question) => {
-					res.sendFile(path.join(__dirname, `assets/${question.getQuestionRelativePath()}`));
-				})
-				.catch((error) => {
-					console.log("Query status " + error);
-					res.send(error);
-				});
+		this.app.get("/question-style.css", async (req, res) => {
+			res.sendFile(path.join(__dirname, `assets/question_html/question-style.css`));
 		});
 
-		this.app.get("/questionFeedbackImage", async (req, res) => {
-			const questionId = req.query.id;
-			const languageShortName = req.query.languageShortName;
-			const schoolGradeId = req.query.schoolGradeId;
+		this.app.get("/question-image/:id", async (req, res) => {
+			const questionId = req.params.id;
+			const fileName = this.renameToSVGFile(questionId);
 
-			this.questionRepo
-				.getQuestionById(Number(questionId), languageShortName.toString(), Number(schoolGradeId))
-				.then((question) => {
-					res.sendFile(path.join(__dirname, `assets/${question.getFeedbackRelativePath()}`));
-				})
-				.catch((error) => {
-					console.log("ORM query status " + error);
-					res.send(error);
-				});
+			res.sendFile(path.join(__dirname, `assets/question_images/${fileName}`));
+		});
+
+		this.app.get("/question-html/:id", (req, res) => {
+			const questionId = req.params.id;
+			const languageShortName = !!req.query.languageShortName ? req.query.languageShortName : "fr";
+
+			res.sendFile(path.join(__dirname, `assets/question_html/questions_html/question_${questionId}_${languageShortName}.html`));
+		});
+
+		this.app.get("/feedback-html/:id", (req, res) => {
+			const feedbackId = req.params.id;
+			const languageShortName = !!req.query.languageShortName ? req.query.languageShortName : "fr";
+
+			res.sendFile(path.join(__dirname, `assets/question_html/feedback_html/feedback_${feedbackId}_${languageShortName}.html`));
+		});
+
+		this.app.get("/answer-html/:id", (req, res) => {
+			const answerId = req.params.id;
+			const languageShortName = !!req.query.languageShortName ? req.query.languageShortName : "fr";
+
+			res.sendFile(path.join(__dirname, `assets/question_html/answers_html/answer_${answerId}_${languageShortName}.html`));
 		});
 
 		this.app.post("/errorReport", jsonParser, (req, res) => {
@@ -72,9 +74,162 @@ export class Server {
 			console.log(body);
 			this.errorRepo.addReportedError(body.languageShortName, body.errorDescription, JSON.stringify(body.errorLog), body.username, body.questionId);
 		});
+
+		//TODO maybe remove it in production
+		this.app.get("/generate-questions-latex-files", async (req, res) => {
+			this.questionRepo
+				.getAllQuestions()
+				.then((questions: any[]) => {
+					const questionPromises: Promise<any>[] = questions.map((question) => this.writeQuestionsLatexFile(question, false));
+					const feedbackPromises: Promise<any>[] = questions.map((question) => this.writeQuestionsLatexFile(question, true));
+
+					Promise.all(questionPromises.concat(feedbackPromises))
+						.then((_) => {
+							console.log("done");
+							res.sendStatus(200);
+						})
+						.catch((err) => {
+							console.error(err);
+							res.sendStatus(err);
+						});
+				})
+				.catch((error) => {
+					console.log("ORM query status " + error);
+					res.send(error);
+				});
+		});
+
+		this.app.get("/generate-answers-latex-files", async (req, res) => {
+			this.questionRepo
+				.getAllAnswers()
+				.then((answers: any[]) => {
+					const answerPromises: Promise<any>[] = answers.map((answer) => this.writeAnswersLatexFile(answer));
+
+					Promise.all(answerPromises)
+						.then((_) => {
+							console.log("done");
+							res.sendStatus(200);
+						})
+						.catch((err) => {
+							console.error(err);
+							res.sendStatus(err);
+						});
+				})
+				.catch((error) => {
+					console.log("ORM query status " + error);
+					res.send(error);
+				});
+		});
+
+		this.app.get("/append-to-html", async (req, res) => {
+			try {
+				await this.appendToHtml("feedback");
+				await this.appendToHtml("questions");
+				await this.appendToHtml("answers");
+
+				res.sendStatus(200);
+			} catch (error) {
+				console.log(error);
+				res.send(error);
+			}
+		});
+
+		this.app.get("/modify-html", async (req, res) => {
+			try {
+				await this.modifyHtmlFiles("feedback");
+				await this.modifyHtmlFiles("questions");
+				await this.modifyHtmlFiles("answers");
+
+				res.sendStatus(200);
+			} catch (error) {
+				console.log(error);
+				res.send(error);
+			}
+		});
+	}
+
+	private renameToSVGFile(questionId: string) {
+		let stringArray = questionId.split(".");
+		const lastElement = stringArray[stringArray.length - 1];
+
+		if (lastElement !== "svg") {
+			if (lastElement === "eps") stringArray.pop();
+
+			stringArray.push("svg");
+		}
+		return stringArray.join(".");
 	}
 
 	public listen(callback: (port: number) => void): void {
 		this.httpServer.listen(this.DEFAULT_PORT, () => callback(this.DEFAULT_PORT));
+	}
+
+	private writeQuestionsLatexFile(question: any, isFeedback: boolean): Promise<any> {
+		const data = isFeedback ? question.feedbackLatex : question.questionLatex;
+		const directory = isFeedback ? "feedback_latex" : "question_latex";
+		const fileName = isFeedback ? "feedback" : "question";
+
+		return fs.promises.writeFile(
+			path.join(__dirname, `assets/latex_files/${directory}/${fileName}_${question.questionId}_${question.shortName}.tex`),
+			data
+		);
+	}
+
+	private writeAnswersLatexFile(answer: any): Promise<any> {
+		return fs.promises.writeFile(
+			path.join(__dirname, `assets/latex_files/answers_latex/answer_${answer.answerId}_${answer.shortName}.tex`),
+			answer.answerLatex
+		);
+	}
+
+	private async appendToHtml(directory: string): Promise<void> {
+		const dirPath = path.join(__dirname, `assets/question_html`);
+		const files = await fs.promises.readdir(`${dirPath}/${directory}_html`);
+
+		const htmlToAppend = await fs.promises.readFile(`${dirPath}/append.html`, { encoding: "utf-8" });
+
+		for (const file of files) {
+			console.log(file);
+			const filePath = `${dirPath}/${directory}_html/${file}`;
+			const htmlToModify = await fs.promises.readFile(filePath, { encoding: "utf-8" });
+			await fs.promises.writeFile(filePath, this.modifyHtml(htmlToModify));
+			await fs.promises.appendFile(filePath, htmlToAppend);
+		}
+	}
+
+	private async modifyHtmlFiles(directory: string): Promise<void> {
+		const dirPath = path.join(__dirname, `assets/question_html`);
+		const files = await fs.promises.readdir(`${dirPath}/${directory}_html`);
+
+		for (const file of files) {
+			console.log(file);
+			const filePath = `${dirPath}/${directory}_html/${file}`;
+			const htmlToModify = await fs.promises.readFile(filePath, { encoding: "utf-8" });
+			await fs.promises.writeFile(filePath, this.modifyHtml(htmlToModify));
+		}
+	}
+
+	private modifyHtml(html: string): string {
+		const dom = new JSDOM(html);
+		dom.window.document.head.innerHTML = `<link rel="stylesheet" href="${process.env.SERVER_API_URL}/question-style.css>"`;
+
+		dom.window.document.querySelectorAll("embed").forEach((element) => {
+			let image = dom.window.document.createElement("img");
+			image.src = element.src;
+			element.parentElement.replaceChild(image, element);
+		});
+
+		dom.window.document.querySelectorAll("img").forEach((element) => {
+			element.src = `${process.env.SERVER_API_URL}/question-image/${this.renameToSVGFile(element.src)}`;
+		});
+
+		dom.window.document.querySelectorAll("u").forEach((element) => {
+			console.log(`inner html: ${element.innerHTML}`);
+			if (element.innerHTML === "") {
+				element.innerHTML = "___";
+			}
+		});
+
+		return dom.window.document.documentElement.innerHTML;
 	}
 }
